c81b777459d427f51ded656fa07bb40a5e82d16e
siva-arun-s <sivaaruns49@gmail.com>
2025-10-08 11:11:54 +0530

    inital commit:basic

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to ShopEasy!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
